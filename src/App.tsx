5149cde23e26850f73a2376bff33bebd2604d239
NEMYSESx <111594052+NEMYSESx@users.noreply.github.com>
2024-01-14 11:56:38 +0530

    added all the functions of toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useRef, useEffect } from "react";
-import Whiteboard from "./components/canvas/Whiteboard";
 import Bar from "./components/bar";
 
 function App() {
   return (
     <div className="relative flex justify-center">
       <Bar />
-      <Whiteboard />
     </div>
   );
 }
--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -1,43 +1,110 @@
-import React, { FC, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import Card from "./ui/cards";
 import circleIcon from "../assets/icons/circle.svg";
 import pencilIcon from "../assets/icons/pencil.svg";
 import squareIcon from "../assets/icons/square.svg";
 import triangleIcon from "../assets/icons/triangle.svg";
 
-const Bar: FC = () => {
-  interface Square {
-    id: number;
-  }
+const Bar: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const [squares, setSquares] = useState<Square[]>([]);
+  const pencil = (e: MouseEvent) => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
 
-  const creatingCircle = () => {
-    console.log("sgfd");
+    if (canvas && context) {
+      context.beginPath();
+      context.moveTo(
+        e.clientX - canvas.offsetLeft,
+        e.clientY - canvas.offsetTop
+      );
+
+      const draw = (e: MouseEvent) => {
+        context.lineTo(
+          e.clientX - canvas.offsetLeft,
+          e.clientY - canvas.offsetTop
+        );
+        context.stroke();
+      };
+
+      const stopDrawing = () => {
+        canvas.removeEventListener("mousemove", draw);
+        canvas.removeEventListener("mouseup", stopDrawing);
+      };
+
+      canvas.addEventListener("mousemove", draw);
+      canvas.addEventListener("mouseup", stopDrawing);
+    }
   };
 
-  const pencil = () => {
-    console.log("retdf");
+  const createCircle = (e: MouseEvent) => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (canvas && context) {
+      const x = e.clientX - canvas.offsetLeft;
+      const y = e.clientY - canvas.offsetTop;
+
+      context.beginPath();
+      context.arc(x, y, 30, 0, 2 * Math.PI);
+      context.stroke();
+    }
   };
 
-  const createSquare = () => {
-    const newSquare: Square = {
-      id: squares.length + 1,
-    };
-    setSquares((prevSquares) => [...prevSquares, newSquare]);
+  const createSquare = (e: MouseEvent) => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (canvas && context) {
+      const x = e.clientX - canvas.offsetLeft;
+      const y = e.clientY - canvas.offsetTop;
+
+      context.beginPath();
+      context.rect(x - 30, y - 30, 60, 60);
+      context.stroke();
+    }
   };
 
-  const creatingTriangle = () => {
-    console.log("xc");
+  const createTriangle = (e: MouseEvent) => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (canvas && context) {
+      const x = e.clientX - canvas.offsetLeft;
+      const y = e.clientY - canvas.offsetTop;
+
+      context.beginPath();
+      context.moveTo(x, y);
+      context.lineTo(x + 60, y);
+      context.lineTo(x + 30, y - 60);
+      context.closePath();
+      context.stroke();
+    }
   };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const context = canvas.getContext("2d");
+
+      canvas.addEventListener("mousedown", pencil);
+      canvas.addEventListener("mousedown", createCircle);
+      canvas.addEventListener("mousedown", createSquare);
+      canvas.addEventListener("mousedown", createTriangle);
+    }
+  }, []);
 
   return (
-    <nav className="border absolute flex">
-      <Card imageSrc={circleIcon} onClick={creatingCircle} />
-      <Card imageSrc={pencilIcon} onClick={pencil} />
-      <Card imageSrc={squareIcon} onClick={createSquare} />
-      <Card imageSrc={triangleIcon} onClick={creatingTriangle} />
-    </nav>
+    <div>
+      <nav className="border absolute flex">
+        <Card imageSrc={circleIcon} onClick={createCircle} />
+        <Card imageSrc={pencilIcon} onClick={pencil} />
+        <Card imageSrc={squareIcon} onClick={createSquare} />
+        <Card imageSrc={triangleIcon} onClick={createTriangle} />
+      </nav>
+      <canvas ref={canvasRef} width={1200} height={800} />
+    </div>
   );
 };
 
